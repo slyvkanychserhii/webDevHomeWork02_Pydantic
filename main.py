b51d6ce44a83774ad256553e3f76a56c6e481266
slyvkanychserhii <slyvkanychserhii@gmail.com>
2024-07-15 01:53:19 +0200

    Fourth commit

--- main.py
+++ main.py
@@ -1,115 +1,112 @@
-# pip install pydantic
-# pip install email-validator  # pip install 'pydantic[email]'
 
-from pydantic import BaseModel, Field, EmailStr, field_validator, ValidationError
+from typing import Self
+from pydantic import (
+    BaseModel,
+    Field,
+    PositiveInt,
+    EmailStr,
+    ValidationError,
+    model_validator
+)
 
 
 class Address(BaseModel):
     city: str = Field(min_length=2)
     street: str = Field(min_length=3)
-    house_number: int = Field(gt=0)
+    house_number: PositiveInt
 
 
 class User(BaseModel):
-    name: str = Field(min_length=2)
+    name: str = Field(pattern="^[A-Za-z]{2,}$")
     age: int = Field(gt=0, lt=120)
     email: EmailStr
     is_employed: bool = False
     address: Address
 
-    @field_validator('name')
-    def check_name(cls, value):
-        if not value.isalpha():
-            raise ValueError('Name must contain only alphabetic characters')
-        return value
-
-    @field_validator('is_employed')
-    def check_employment_age(cls, value, values):
-        age = values.data.get('age')
-        if value and age is not None and age < 18:
-            raise ValueError('User must be at least 18 years old to be employed')
-        return value
-
-    @classmethod
-    def ich_model_validate_json(cls, json_string):
-        try:
-            user = cls.model_validate_json(json_string)
-            return user.json()
-        except ValidationError as e:
-            return e.json()
+    @model_validator(mode="after")
+    def check_employment_age(self) -> Self:
+        is_employed = self.is_employed
+        age = self.age
+        if is_employed and not (18 <= age <= 65):
+            raise ValueError("Employed users must be between 18 and 65 years old")
+        return self
 
 
-valid_json_1 = """{
-    "name": "Serhii",
-    "age": 40,
-    "email": "serhii@example.com",
-    "is_employed": true,
-    "address": {
-        "city": "Mülheim an der Ruhr",
-        "street": "Aktienstraße",
-        "house_number": 123
-    }
-}"""
+def user_validate_json(user_json):
+    try:
+        user = User.model_validate_json(user_json)
+        return user.model_dump_json()
+    except ValidationError as e:
+        print(e)
 
-valid_json_2 = """{
-    "name": "Alice",
-    "age": 16,
-    "email": "alice@example.com",
-    "address": {
-        "city": "Mülheim an der Ruhr",
-        "street": "Aktienstraße",
-        "house_number": 123
-    }
-}"""
 
-invalid_name_json = """{
-    "name": "serhii_123",
-    "age": 40,
-    "email": "serhii@example.com",
-    "address": {
-        "city": "Mülheim an der Ruhr",
-        "street": "Aktienstraße",
-        "house_number": 123
-    }
-}"""
+new_user_json_1 = """
+{"name": "Serhii",
+"age": 40,
+"email": "serhii@example.com",
+"is_employed": true,
+"address": {
+"city": "Mülheim an der Ruhr",
+"street": "Aktienstraße",
+"house_number": 123}}
+"""
+new_user_1 = user_validate_json(new_user_json_1)
+print(new_user_1)
 
-invalid_age_json = """{
-    "name": "Serhii",
-    "age": 130,
-    "email": "serhii@example.com",
-    "address": {
-        "city": "Mülheim an der Ruhr",
-        "street": "Aktienstraße",
-        "house_number": 123
-    }
-}"""
+new_user_json_2 = """
+{"name": "Alice",
+"age": 16,
+"email": "alice@example.com",
+"address": {
+"city": "Mülheim an der Ruhr",
+"street": "Aktienstraße",
+"house_number": 123}}
+"""
+new_user_2 = user_validate_json(new_user_json_2)
+print(new_user_2)
 
-invalid_email_json = """{
-    "name": "Serhii",
-    "age": 40,
-    "email": "serhiiexample.com",
-    "address": {
-        "city": "Mülheim an der Ruhr",
-        "street": "Aktienstraße",
-        "house_number": 123
-    }
-}"""
+new_user_json_invalid_name = """
+{"name": "serhii_123",
+"age": 40,
+"email": "serhii@example.com",
+"address": {
+"city": "Mülheim an der Ruhr",
+"street": "Aktienstraße",
+"house_number": 123}}
+"""
+new_user_3 = user_validate_json(new_user_json_invalid_name)
 
-invalid_employment_age_json = """{
-    "name": "Serhii",
-    "age": 16,
-    "email": "serhii@example.com",
-    "is_employed": true,
-    "address": {
-        "city": "Mülheim an der Ruhr",
-        "street": "Aktienstraße",
-        "house_number": 123
-    }
-}"""
+new_user_json_invalid_age = """
+{"name": "Serhii",
+"age": 130,
+"email": "serhii@example.com",
+"address": {
+"city": "Mülheim an der Ruhr",
+"street": "Aktienstraße",
+"house_number": 123}}
+"""
+new_user_4 = user_validate_json(new_user_json_invalid_age)
 
-print(User.ich_model_validate_json(valid_json_1))
-print(User.ich_model_validate_json(valid_json_2))
-print(User.ich_model_validate_json(invalid_name_json))
-print(User.ich_model_validate_json(invalid_age_json))
-print(User.ich_model_validate_json(invalid_email_json))
-print(User.ich_model_validate_json(invalid_employment_age_json))
+new_user_json_invalid_email = """
+{"name": "Serhii",
+"age": 40,
+"email": "serhiiexample.com",
+"address": {
+"city": "Mülheim an der Ruhr",
+"street": "Aktienstraße",
+"house_number": 123
+}}
+"""
+new_user_5 = user_validate_json(new_user_json_invalid_email)
+
+new_user_json_invalid_employment_age = """
+{"name": "Serhii",
+"age": 16,
+"email": "serhii@example.com",
+"is_employed": true,
+"address": {
+"city": "Mülheim an der Ruhr",
+"street": "Aktienstraße",
+"house_number": 123}}
+"""
+new_user_6 = user_validate_json(new_user_json_invalid_employment_age)
